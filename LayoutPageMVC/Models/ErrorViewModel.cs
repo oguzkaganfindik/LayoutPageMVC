47247ad2d01567bab3fbad6eba798f21340738e8
Oguzkagan Findik <oguzfindik14@gmail.com>
2023-07-24 02:25:31 +0300

    Add project files.

---
+++ LayoutPageMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LayoutPageMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
